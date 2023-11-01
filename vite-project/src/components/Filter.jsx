1af1240dbd3841686baee46b371328f52918356e
Andrea Cabrera Robles <marisoljbnick@hotmail.com>
2023-11-01 16:34:29 -0600

    Funcionalidad HU2

--- vite-project/src/components/Filter.jsx
+++ vite-project/src/components/Filter.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const Filter = () => {
+const Filter = ({onFilterByChange, selectedFilter}) => {
+
   return (
-    <select className='selector'>
-        <option disabled selected>Filter by</option>
-        <option value={"romance"}>Romance</option>
-        <option value={"comedy"}>Comedy</option>
-        <option value={"drama"}>Drama</option>
-        <option value={"family"}>Family</option>
-        <option value={"animation"}>Animation</option>
+    <select className='selector' onChange={onFilterByChange} value={selectedFilter}>
+        <option defaultValue>Filter by genre</option>
+        <option value="10749">Romance</option>
+        <option value="35">Comedy</option>
+        <option value="18">Drama</option>
+        <option value="10751">Family</option>
+        <option value="16">Animation</option>
+        <option value="12">Adventure</option>
+        <option value="14">Fantasy</option>
+        <option value="10402">Music</option>
+        <option value="10770">TV Movie</option>
     </select>
   )
 }
